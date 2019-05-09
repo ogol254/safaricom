7dfa95b09536423db7cfef140a277c29e5de50de
masterabram <abramogol@gmail.com>
2019-05-09 08:53:01 +0300

    project set up

---
+++ instance/config.py
@@ -0,0 +1,39 @@
+"""
+This module sets the configurations for the application
+"""
+import os
+
+
+class Config(object):
+    """Parent configuration class."""
+    DEBUG = False
+    CSRF_ENABLED = True
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.getenv("SECRET_KEY", "kalongo")
+    DATABASE_TEST_URL = os.getenv("DATABASE_TEST_URL")
+
+
+class DevelopmentConfig(Config):
+    """Development phase configurations"""
+    DEBUG = True
+    TESTING = True
+
+
+class TestingConfig(Config):
+    """Testing Configurations."""
+    DEBUG = True
+    TESTING = True
+    DATABASE_URL = os.getenv("DATABASE_TEST_URL")
+
+
+class ReleaseConfig(Config):
+    """Release Configurations."""
+    DEBUG = False
+    TESTING = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'release': ReleaseConfig,
+}
